549490215ca020f7ecbbaa031c6c71058d730b75
Hurtony Péter <hurtonypeter@gmail.com>
2014-03-29 13:44:39 +0100

    User entity data modeljéhez hozzáadva a FullName számított érték

---
+++ Perseus/DataModel/PerseusPartial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Perseus.DataModel
+{
+    public partial class User
+    {
+        public string FullName
+        {
+            get { return this.LastName + " " + this.FirstName; }
+        }
+    }
+}
