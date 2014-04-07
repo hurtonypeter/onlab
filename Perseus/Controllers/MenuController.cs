023518831aab28badbd0bbe356d54e496da22a21
Hurtony Péter <hurtonypeter@gmail.com>
2014-04-07 23:31:56 +0200

    MenuViewModel, Menu partialview hozzáadva, menucontrollerben a főmenü implementálva

--- Perseus/Controllers/MenuController.cs
+++ Perseus/Controllers/MenuController.cs
@@ -1,4 +1,6 @@
 ﻿using Perseus.DataModel;
+using Perseus.Models;
+using Perseus.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +14,39 @@ namespace Perseus.Controllers
         protected Entities db = new Entities();
         //
         // GET: /Menu/
-        public ActionResult Index()
+        public PartialViewResult MainMenu()
         {
-            var menu = db.MenuItem.ToList();
+            var mainmenu = db.Menu.SingleOrDefault(m => m.Name.Equals("main-menu"))
+                .MenuItem.Where(s => s.Parent == null).ToList();
 
-            return View();
+            List<MenuViewModel> model = new List<MenuViewModel>();
+
+            foreach (var item in mainmenu)
+            {
+                MenuViewModel menu = new MenuViewModel
+                {
+                    Path = item.LinkPath,
+                    Title = item.LinkTitle
+                };
+                if(item.Children != null)
+                {
+                    foreach (var child in item.Children)
+                    {
+                        if(AccountHelper.HasPermission(child.Permission.Name))
+                        {
+                            menu.Children.Add(new MenuViewModel
+                            {
+                                Path = child.LinkPath,
+                                Title = child.LinkTitle
+                            });
+                        }
+                    }
+                }
+                model.Add(menu);
+            }
+
+            return PartialView("~/Views/Shared/_Menu.cshtml", model);
         }
+
 	}
 }
---
+++ Perseus/Models/MenuModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Perseus.Models
+{
+    public class MenuViewModel
+    {
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public string Classes { get; set; }
+        public List<MenuViewModel> Children { get; set; }
+        public MenuViewModel()
+        {
+            Children = new List<MenuViewModel>();
+        }
+    }
+}
