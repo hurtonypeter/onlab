568522e0c36e53c7283e77aa95409070ef7bd267
Hurtony Péter <hurtonypeter@gmail.com>
2014-04-15 00:41:12 +0200

    htmlhelper hozzáadva a relatív linkből abszolut generálásához

--- Perseus/Helpers/RazorHelperExtensions.cs
+++ Perseus/Helpers/RazorHelperExtensions.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Perseus.Helpers
 {
-    public static class UriHelperExtensions
+    public static class RazorHelperExtensions
     {
         // Prepend the provided path with the scheme, host, and port of the request.
         public static string FormatAbsoluteUrl(this Uri url, string path)
@@ -19,6 +20,16 @@ namespace Perseus.Helpers
         {
             return string.Format("{0}://{1}{2}", url.Scheme,
                url.Host, url.Port == 80 ? string.Empty : ":" + url.Port);
+        }
+        public static string LinkFromRelative(this HtmlHelper helper, string relativePath)
+        {
+            if (relativePath.StartsWith("/"))
+            {
+                relativePath = relativePath.TrimStart(new Char[] { '/' });
+            }
+            var url = HttpContext.Current.Request.Url;
+
+            return url.Scheme + "://" + url.Authority + "/" + relativePath;
         }
     }
 }
